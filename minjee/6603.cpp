e8779e84a341b6211236854c3ce8a8335e135d2e
minjee12 <35055320+minjee12@users.noreply.github.com>
2021-07-31 22:51:23 +0900

    로또_재귀

---
+++ minjee/6603.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int k;
+int s[6];  //b
+
+void lotto(int m[12], int a, int b) {
+	if (b == 6) {
+		for (int i = 0; i < 6; i++) {
+			cout << s[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = a; i < k; i++) {
+		s[b] = m[i];
+		lotto(m, i + 1, b + 1);
+	}
+}
+
+int main() {
+	int arr[12];  //a
+	while (true) {
+		cin >> k;
+		if (k == 0) break;
+		for (int i = 0; i < k; i++) {
+			cin >> arr[i];
+		}
+
+		lotto(arr, 0, 0);
+		memset(arr, 0, sizeof(arr));
+		cout << "\n";
+
+	}
+	return 0;
+}
