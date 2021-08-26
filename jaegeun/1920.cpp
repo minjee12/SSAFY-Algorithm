648934538a95a7db4324dd4c66500a6131665226
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-08-26 17:46:35 +0900

    수 찾기

---
+++ jaegeun/1920.cpp
@@ -0,0 +1,32 @@
+﻿// 수 찾기
+// https://www.acmicpc.net/problem/1920
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+
+	int n; cin >> n;
+
+	vector<int> A(n);
+	for (int i = 0; i < n; i++) cin >> A[i];
+
+	sort(A.begin(), A.end());
+
+	int m; cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int num; cin >> num;
+		cout << binary_search(A.begin(), A.end(), num) << endl;
+	}
+	return 0;
+}
