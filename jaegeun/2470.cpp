9f672c9ca48590d8f02a817d67b39e1673c59679
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-08-29 22:55:29 +0900

    두 용액

---
+++ jaegeun/2470.cpp
@@ -0,0 +1,38 @@
+﻿// 두 용액
+// https://www.acmicpc.net/problem/2470
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+	int N; cin >> N;
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++) cin >> arr[i];
+
+	sort(arr.begin(), arr.end());
+
+	int st = 0, ed = N - 1;
+	int min_result = 21e8;
+	int result1 = 0, result2 = 0;
+	while (true)
+	{
+		if (st >= ed) break;
+		int sum = arr[st] + arr[ed];
+		if (min_result > abs(sum))
+		{
+			min_result = abs(sum);
+			result1 = arr[st];
+			result2 = arr[ed];
+		}
+		if (sum == 0) break;
+		if (sum < 0) st++;
+		else ed--;
+	}
+	cout << result1 << " " << result2 << endl;
+	return 0;
+}
