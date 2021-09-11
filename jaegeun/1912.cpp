b45e10e7cb7e07ca2972c7b9c3f31106e07a35f0
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-09-11 18:56:39 +0900

    연속합

---
+++ jaegeun/1912.cpp
@@ -0,0 +1,29 @@
+﻿// 연속합
+// https://www.acmicpc.net/problem/1912
+
+#include <iostream>
+#define MAX 100000
+
+using namespace std;
+
+int arr[MAX];
+int dp[MAX];
+
+int main()
+{
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	dp[0] = arr[0];
+
+	int result = dp[0];
+	for (int i = 1; i < n; i++)
+	{
+		dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+		result = max(dp[i], result);
+	}
+
+	cout << result << '\n';
+	return 0;
+}
