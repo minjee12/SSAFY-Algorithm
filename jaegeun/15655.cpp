68fafce3afa472ad8177de29fbe10cab859fca16
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-09-18 23:39:04 +0900

    N 과 M 시리즈

---
+++ jaegeun/15655.cpp
@@ -0,0 +1,44 @@
+﻿// N과 M (6)
+// https://www.acmicpc.net/problem/15655
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MAX 8
+using namespace std;
+
+int M = 0, N = 0;
+
+int element[MAX];
+int used[MAX];
+
+void dfs(int level, int index)
+{
+	if (level == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << used[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = index; i < N; i++)
+	{
+		used[level] = element[i];
+		dfs(level + 1, i + 1);
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> element[i];
+	}
+	sort(element, element + N);
+	dfs(0, 0);
+}
