9cb72a54f9312a232e039ddc940dc057ede09820
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-10-27 00:52:10 +0900

    소가 길을 건너간 이유 3(21-10-26)

---
+++ jaegeun/14469.cpp
@@ -0,0 +1,29 @@
+﻿// 소가 길을 건너간 이유 3
+// https://www.acmicpc.net/problem/14469
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+	int N; cin >> N;
+
+	vector<pair<int, int>> cows(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> cows[i].first >> cows[i].second;
+	}
+	sort(cows.begin(), cows.end());
+
+	int total_time = 0;
+	for (int i = 0; i < N; i++) {
+		if (total_time < cows[i].first) total_time += (cows[i].first - total_time + cows[i].second);
+		else total_time += cows[i].second;
+	}
+	cout << total_time;
+	return 0;
+
+}
