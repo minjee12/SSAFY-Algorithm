236aae31768821d30258826e0a4c7ffeebb0753a
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-12-06 18:40:56 +0900

    IOIOI(21-12-06)

---
+++ jaegeun/~12-08/5525.cpp
@@ -0,0 +1,54 @@
+﻿// IOIOI
+// https://www.acmicpc.net/problem/5525
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, M;
+string S;
+int main() {
+	freopen_s(new FILE*, "text.txt", "r", stdin);
+	cin >> N >> M >> S;
+	int total_cnt = 0;
+	// IOIOIOI의 길이를 구함
+	// N의 개수에 따라 개수 계산
+	// 다음으로 넘어감
+	bool is_ioi = false;
+	int o_cnt = 0;
+	for (int i = 0; i < S.length(); i++) {
+		if (S[i] == 'O' && i + 1 == M) break;
+		// IOIOI 시작 조건
+		if (S[i] == 'I' && S[i + 1] == 'O' && !is_ioi) { 
+			is_ioi = true;
+			o_cnt = 0;
+		}
+		// IOIOI 종료 조건
+		if (S[i] == 'O' && is_ioi) {
+			if (S[i + 1] == 'O') {
+				is_ioi = false;
+				if (o_cnt >= N) {
+					int ioi_cnt = o_cnt - N + 1;
+					total_cnt += ioi_cnt;
+				}
+			}
+		}
+		if (S[i] == 'I' && is_ioi && S[i + 1] == 'I') {
+			is_ioi = false;
+			if (o_cnt >= N) {
+				int ioi_cnt = o_cnt - N + 1;
+				total_cnt += ioi_cnt;
+			}
+		}
+		if (S[i] == 'O' && is_ioi) o_cnt++;
+	}
+	if (is_ioi) {
+		if (o_cnt >= N) {
+			int ioi_cnt = o_cnt - N + 1;
+			total_cnt += ioi_cnt;
+		}
+	}
+	cout << total_cnt;
+	return 0;
+}
