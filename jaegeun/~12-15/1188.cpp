af9d9c608e4e0d0f4a2470428efba84b3b39f743
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-12-12 20:57:44 +0900

    음식평론가 참고(21-12-12)

---
+++ jaegeun/~12-15/1188.cpp
@@ -0,0 +1,20 @@
+﻿// 음식 평론가
+// https://www.acmicpc.net/problem/1188
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int x, int y) {
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+int main() {
+    freopen_s(new FILE*, "text.txt", "r", stdin);
+    int N, M; cin >> N >> M;
+    cout << M - gcd(N, M);
+
+    return 0;
+}
