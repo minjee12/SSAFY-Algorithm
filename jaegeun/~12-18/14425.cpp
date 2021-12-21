c7cbeb98bb3d023cfe7f4c469334023b0849e418
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-12-21 20:18:06 +0900

    문자열 집합(21-12-21)

---
+++ jaegeun/~12-18/14425.cpp
@@ -0,0 +1,25 @@
+﻿// 문자열 집합
+// https://www.acmicpc.net/problem/14425
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, m; cin >> n >> m;
+    map<string, int> S;
+    for (int i = 0; i < n; i++) {
+        string str; cin >> str;
+        S[str] = 1;
+    }
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        string str; cin >> str;
+        if (S[str] == 1) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
