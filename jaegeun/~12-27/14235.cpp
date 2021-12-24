d18243df486182c2090b1ba4e1a2e0198aa69d76
이재근 <49888128+dlworms32@users.noreply.github.com>
2021-12-24 22:45:40 +0900

    크리스마스 선물(21-12-24)

---
+++ jaegeun/~12-27/14235.cpp
@@ -0,0 +1,30 @@
+﻿// 크리스마스 선물
+// https://www.acmicpc.net/problem/14235
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+	priority_queue<int> pq;
+	for (int i = 0; i < N; i++) {
+		int a; cin >> a;
+		if (a == 0) {
+			if (pq.size() == 0) cout << -1 << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			for (int j = 0; j < a; j++) {
+				int n; cin >> n;
+				pq.push(n);
+			}
+		}
+	}
+	return 0;
+}
